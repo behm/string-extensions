588f79c5e0443150f1068bdd3803e9df8cd5d134
Brian Behm <brian.behm@gmail.com>
2021-09-21 21:06:50 -0500

    Added two methods to deal with null and empty strings;

---
+++ Behm.Extensions.Strings.Tests/StringExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿using Behm.Extensions.Strings;
+using FluentAssertions;
+using Xunit;
+
+namespace Behm.Extension.Strings.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void StringEmpty_EqualsEmptyDoubleQuotes()
+        {
+            // Arrange
+            var testEmptyString = string.Empty;
+            var testDoubleQuoteEmptyString = "";
+
+            // Act, Assert
+            Assert.Equal(testEmptyString, testDoubleQuoteEmptyString);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", null)]
+        [InlineData(" ", null)]
+        [InlineData("   ", null)]
+        [InlineData("some value", "some value")]
+        public void GetNullIfEmpty_GivenValue_ReturnsExpectedResult(string testString, string expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = testString.GetNullIfEmptyString();
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData(" ", " ")]
+        [InlineData("non-empty string", "non-empty string")]
+        public void GetEmptyStringIfNull_GivenValue_ReturnsExpectedResult(string testString, string expectedResult)
+        {
+            // Arrange
+            
+            // Act
+            var result = testString.GetEmptyStringIfNull();
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ Behm.Extensions.Strings/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Behm.Extensions.Strings
+{
+    public static class StringExtensions
+    {
+        public static string GetNullIfEmptyString(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return null;
+            }
+
+            return str;
+        }
+
+        public static string GetEmptyStringIfNull(this string str)
+        {
+            if (str == null)
+            {
+                return "";
+            }
+
+            return str;
+        }
+    }
+}
